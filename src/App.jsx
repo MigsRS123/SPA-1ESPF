6ec988034263e230676c854fa8794ef78603e61c
MigsRS123 <rm551239@fiap.com.br>
2023-09-04 09:54:59 -0300

    Props + Componentização

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import Cabecalho from "./components/Cabecalho";
 import Conteudo from "./components/Conteudo";
 import Rodape from "./components/Rodape";
+import viteLogo from "./assets/vite.svg";
 
 
 export default function App(){
+
+  let viteLogoAlt = "Vite Logo"
   
   return(
     <>
@@ -13,7 +16,8 @@ export default function App(){
         <Cabecalho/>
 
         {/* Iniciando a área do conteúdo */}
-       <Conteudo/>
+        <Conteudo viteLogoProps={viteLogo} 
+        viteLogoAltProps={viteLogoAlt}/>
 
         {/* Iniciando a area do rodape */}
         <Rodape />
