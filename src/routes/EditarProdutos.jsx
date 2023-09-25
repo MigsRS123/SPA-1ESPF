7127fa72a434ac0fb412ee61b25f6af478d995e3
MigsRS123 <rm551239@fiap.com.br>
2023-09-25 09:38:36 -0300

    modificações

--- src/routes/EditarProdutos.jsx
+++ src/routes/EditarProdutos.jsx
@@ -1,16 +1,21 @@
 import React from 'react'
 import { useParams } from 'react-router-dom'
+import { ListaProduto } from '../components/ListaProdutos';
 
 export default function EditarProdutos() {
 
-    document.title = "Editar Produtos";
-
     const {id} = useParams();
+
+    document.title = "Editar Produtos " + id;
+
+    //Criar uma estratégia para recuperar o produto na lista
+    // Utilizando o id
+    const produtoRecuperadoPorId = ListaProduto.filter(item => item.id == parseInt(id));
 
   return (
     <div>
         <h1>Editar Produtos</h1>
-        <h2>Produto selecinado : {id}</h2>
+        <h2>Produto selecionado : {id}</h2>
         
     </div>
   )
